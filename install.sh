93d45a24402f41a81749adcb2621717b9ded92c4
Isaac Lewis <isaac.b.lewis@gmail.com>
2019-10-10 06:42:44 -0400

    Updage comments; use  in place of 'set -e' to simplify debugging.

--- install.sh
+++ install.sh
@@ -2,27 +2,14 @@
 
 # my-geo-ip -- The my-geo-ip package provides geo-ip services to
 #              applications via a MySQL database.
-# Copyright (C) 2016 Isaac Lewis
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2016-2019 Isaac Lewis
 
 ### Install my-geo-ip.
 
 ## Setup environment variables and error handling.
 
-# Exit immediately if a pipeline exits with a non-zero status.
-set -e
+$bash_set_cmd
 
 # Set the shell options.
 shopt -s extglob
@@ -90,7 +77,9 @@ chown -R mysql:mysql $mysql_my_geo_ip_dir
 # Generate the MySQL passwords.
 source build-passwords.sh
 
-# Generate the MySQL config for the system user root.
+# Generate the MySQL config for the system user root.  When debugging,
+# make sure that all shell environment variables have been loaded
+# before running the following commands.
 envsubst < templates/root.my.cnf.tp  > "/root/.my.cnf"
 
 # Generate the MySQL config for the system user geo-ip.
@@ -100,14 +89,15 @@ chown geo-ip /home/geo-ip/.my.cnf
 # Include this for 'mysql_start'.
 source lib-mysql-helpers.sh
 
-# Initialize MySQL.
+# Initialize MySQL; this doesn't start a MySQL server that listens for
+# connections.
 mysqld --initialize-insecure
 
 ##
 
 ## Install the geo-ip database.
 
-# Start the MySQL server.
+# Start the MySQL server and listen for connections.
 mysql_start
 
 # Create the empty geo-ip database.
